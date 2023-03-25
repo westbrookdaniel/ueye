dee04e758f3124dc9f4bb2249613624ff070c2b6
westy12dan <westy12dan@gmail.com>
2023-03-25 09:38:04 +0800

    feat(init): Initial boiler

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import fs from 'fs'
+import path from 'path'
+import Jimp from 'jimp'
+import { Command } from 'commander'
+
+async function processImageData(
+  imageData: Jimp,
+  componentMap: any
+): Promise<string> {
+  // Add your implementation code here
+  // This function should preprocess the image, run it through the machine learning model,
+  // identify the component boxes and types, and generate React code based on the hierarchy
+  return JSON.stringify(imageData) + JSON.stringify(componentMap)
+}
+
+async function generateReactCode(imagePath: string, componentMapPath: string) {
+  try {
+    // Load the image data using Jimp
+    const imageData = await Jimp.read(imagePath)
+
+    // Load the component map from the specified file path
+    const componentMap = require(path.resolve(componentMapPath)).default
+
+    // Process the image data and generate React code
+    const reactCode = await processImageData(imageData, componentMap)
+
+    // Print the generated React code to the console
+    console.log(reactCode)
+  } catch (err) {
+    console.error(
+      `Error: ${err instanceof Error ? err.message : JSON.stringify(err)}`
+    )
+  }
+}
+
+const program = new Command()
+
+program
+  .name('ueye')
+  .description('A CLI tool to generate React code from website images.')
+  .argument('<imagePath>', 'Path to the image file')
+  .argument('<componentMapPath>', 'Path to the component map file')
+  .action((imagePath, componentMapPath) => {
+    // Validate the image file exists
+    if (!fs.existsSync(imagePath)) {
+      console.error(`Error: Image file ${imagePath} not found.`)
+      program.help()
+      process.exit(1)
+    }
+
+    // Validate the component map file exists
+    if (!fs.existsSync(componentMapPath)) {
+      console.error(`Error: Component map file ${componentMapPath} not found.`)
+      program.help()
+      process.exit(1)
+    }
+
+    // Call the function to generate React code
+    generateReactCode(imagePath, componentMapPath)
+  })
+
+program.parse(process.argv)
